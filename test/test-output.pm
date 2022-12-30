122f31be6c22edf6182a0b7a2c0b915803bab66f
docdnp <contactme@via-github.please>
2022-12-30 16:51:57 +0100

    implemented previously skipped test dealing with HT*_SHOW_ALL

--- test/test-output.pm
+++ test/test-output.pm
@@ -138,59 +138,75 @@ subtest "Default: Don't complete on shell pipes or redirects" => sub {
     Test::Output::teardown();
 };
 
-subtest "Config: Return commands and options on HT_SHOW_ALL" => sub {
-    TestFlow::test_to_be_implemented(); return;
-    Test::Output::setup(1);
-    my $cntCmdsAndOpts = sub { my $cliargs = $_[0];
-        my $allopts = helptext::completer::list_matches($cliargs);
-        is(scalar(@$allopts), 6, "Call: $cliargs->[0] '".join("' '",@$cliargs[1..@$cliargs])."': 6 commands and options are returned");
-        my ($cntCmds, $cntOpts);
-        for(@$allopts){ 
-            $_->{isCmd} && $cntCmds++ && next;
-            $cntOpts++ 
-        }
-        is($cntCmds, 3, "Call: ".join(' ',@$cliargs).": 3 commands are returned");
-        is($cntOpts, 3, "Call: ".join(' ',@$cliargs).": 3 options are returned");
+subtest "Config: Return commands and options on HT*_SHOW_ALL" => sub {
+    subtest "Config: Return commands and options on HT_SHOW_ALL" => sub {
+        Test::Output::setup({SHOW_ALL => 1});
+        testOption_SHOW_ALL();
+        Test::Output::teardown(1);
     };
-
-    subtest "Return commands and options" => sub {
-        $cntCmdsAndOpts->(["dummycmd", ''])
-    };
-    subtest "Last option needs one mandatory argument" => sub {
-        $cntCmdsAndOpts->(["dummycmd", '--opt-1-arg', 'ABC', ''])
-    };
-    subtest "Last option needs two mandatory argument" => sub {
-        $cntCmdsAndOpts->(["dummycmd", '--opt-1-arg', 'ABC', 'DEF', ''])
-    };
-    subtest "Last option ignores has optional arguments" => sub {
-        $cntCmdsAndOpts->(["dummycmd", '--opt-1--opt-arg', 'ABC', ''])
-    };
-    subtest "Last option ignores optional argument" => sub {
-        $cntCmdsAndOpts->(["dummycmd", '--opt-1--opt-arg', ''])
-    };
-    Test::Output::teardown(1);
+    subtest "Config: Return commands and options on HT_APP_<APPNAME>_SHOW_ALL" => sub {
+        Test::Output::setup({APP_DUMMYCMD_SHOW_ALL => 1});
+        testOption_SHOW_ALL();
+        Test::Output::teardown(1);
+    }
 };
 
 # ---------------------------------------------------------
-sub setup { my $reload = $_[0];
-    if ($reload) {
-        $ENV{HT_SHOW_ALL} = 1;
-        unittests::load_completer;
+my @env;
+
+sub setup { my $env = $_[0];
+    while (my ($e, $v) = each %$env) {
+        push @env, $e;
+        $ENV{'HT_'.$e} = $v;
     }
+    @env>0 && unittests::load_completer();
     no strict;
     no warnings;
     *{helptext::completer::find_options_bak} = \&{helptext::completer::find_options};
     *{helptext::completer::find_options} = \&createCommandsAndOptions;
 }
 
-sub teardown { my $reload = $_[0];
-    if ($reload) {
-        delete $ENV{HT_SHOW_ALL};
-        unittests::load_completer;
-    }
+sub teardown { 
+    my $reload = @env>0;
+    for (@env) { delete $ENV{'HT_'.$_} }
+    $reload && unittests::load_completer();
     no strict;
     no warnings;
     *{helptext::completer::find_options} = \&{helptext::completer::find_options_bak};
+}
+
+sub testOption_SHOW_ALL {
+    my $cntCmdsAndOpts = sub { my $cliargs = $_[0];
+        my $allopts = helptext::completer::list_matches($cliargs);
+        is(scalar(@$allopts), 7, "Call: $cliargs->[0] '".join("' '",@$cliargs[1..@$cliargs-1])."': 7 commands and options are returned");
+        my ($cntCmds, $cntOpts);
+        for(@$allopts){ 
+            $_->{isCmd} && ++$cntCmds && next;
+            $cntOpts++ 
+        }
+        is($cntCmds, 3, "Call: ".join(' ',@$cliargs).": 3 commands are returned");
+        is($cntOpts, 4, "Call: ".join(' ',@$cliargs).": 4 options are returned");
+    };
+
+    subtest "Return commands and options" => sub {
+        $cntCmdsAndOpts->(["dummycmd", ''])
+    };
+    subtest "Last option has one mandatory argument" => sub {
+        $cntCmdsAndOpts->(["dummycmd", '--opt-1-arg', 'ABC', ''])
+    };
+    subtest "Last option has two mandatory arguments" => sub {
+        $cntCmdsAndOpts->(["dummycmd", '--opt-1-arg', 'ABC', 'DEF', ''])
+    };
+    subtest "Last option has all optional arguments" => sub {
+        $cntCmdsAndOpts->(["dummycmd", '--opt-1-opt-arg', 'ABC', ''])
+    };
+    subtest "Last option has none of all optional arguments" => sub {
+        $cntCmdsAndOpts->(["dummycmd", '--opt-1-opt-arg', ''])
+    };
+    subtest "Last option doesn't need arguments" => sub {
+        $cntCmdsAndOpts->(["dummycmd", '--opt-1--opt-arg', ''])
+    };
+
 }
 
 sub createCommandsAndOptions {
@@ -204,7 +220,7 @@ sub createCommandsAndOptions {
         new Option ({ %$cmd,  name => 'cmd-1' }),
         new Option ({ %$cmd,  name => 'cmd-2' }),
         new Option ({ %$cmd,  name => 'cmd-3' }),
-    ];
+    ], {} , {envns => 'dummycmd'};
 }
 
 1;
