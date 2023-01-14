3af7fa6100d10a74cafdda70750aea290c024e41
docdnp <contactme@via-github.please>
2023-01-14 17:06:34 +0100

    Better guessing of descriptions and the right help format to use * Simplified and fixed some issues regarding an option's detection of its description * Added a feature "Guess Help Opt" in order to choose automatically wether man,   --help or -h is the best option to use

--- test/test-fix-description.pm
+++ test/test-fix-description.pm
@@ -3,46 +3,6 @@ use Test::More;
 use strict;
 use warnings;
 
-subtest "Find unknown description in known opts" => sub {
-    setup();
-    my $optstore  = newOptsStore();
-
-    is(scalar(@{$optstore->{allopts}}), 4, 
-        "Testing Setup: Expecting 4 items in optlist. Actual: "
-        .scalar(@{$optstore->{allopts}}));
-    is($optstore->{allopts}->[-1]->{type}, 'CMD', 
-        "Testing Setup: Expecting optstore[3=-1] being type command (CMD) and ignored. Actual: "
-        .$optstore->{allopts}->[-1]->{type} );
-    is($optstore->{allopts}->[-2]->{type}, 'CMD', 
-        "Testing Setup: Expecting optstore[2=-2] being type command (CMD) and ignored. Actual: "
-        .$optstore->{allopts}->[-2]->{type} );
-    is($optstore->{allopts}->[-3]->{type}, 'LONG', 
-        "Testing Setup: Expecting optstore[1=-3] being type long opt (LONG). Actual: "
-        .$optstore->{allopts}->[-3]->{type} );
-    is($optstore->{allopts}->[-3]->{pad2Desc}, 25, 
-        "Testing Setup: Expecting pad2desc of 25 in optstore[1=-3]. This option will be used. Actual: "
-        .$optstore->{allopts}->[1]->{pad2Desc} );
-
-    my ($allopts) = helptext::completer::find_options(['dummycmd', '-'], 'dummycmd', '--help');
-
-    is(scalar(@$allopts), 6, 
-        "Expecting 6 option. Actual: "
-        .scalar(@$allopts));
-    is($allopts->[-1]->{name}, '--foo', 
-        "Expecting option --foo at end (-1) of detected optlist. Actual: "
-        .$allopts->[-1]->{name});
-    is($allopts->[-2]->{name}, '--test', 
-        "Expecting option --test one before end (-2) of detected optlist. Actual: "
-        .$allopts->[-2]->{name});
-    is($allopts->[-1]->{pad2Desc}, $optstore->{allopts}->[1]->{pad2Desc}, 
-        "Expecting option --foo with pad2desc=".$optstore->{allopts}->[1]->{pad2Desc}.". Actual: "
-        .$allopts->[-1]->{pad2Desc});
-    is($allopts->[-2]->{pad2Desc}, $optstore->{allopts}->[1]->{pad2Desc}, 
-        "Expecting option --test with pad2desc=".$optstore->{allopts}->[1]->{pad2Desc}.". Actual: "
-        .$allopts->[-2]->{pad2Desc});
-    teardown();
-};
-
 subtest "Find description in following lines" => sub {
     setup();
     clearOptlist();
